24181dd46f0b5183379eadfe2cf457dd179579df
Arya-Jain <89778433+Arya-Jain@users.noreply.github.com>
2021-12-10 21:45:31 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ function take_snapshot(){
 
 console.log("ml5 version : ",ml5.version);
 
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/UBwLBlZZ5/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/e7Do1Aeos/model.json', modelLoaded);
 
 function modelLoaded(){
     console.log("model Ready !!!");
@@ -37,6 +37,54 @@ function speak(){
 }
 
 function check(){
-    speak();
+    img = document.getElementById("captured");
+    classifier.classify(img, gotResults);
 }
 
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        document.getElementById("result_gesture_name2").innerHTML = results[1].label;
+
+        prediction_1 = results[0].label;
+        prediction_2 = results[1].label;
+
+        speak();
+
+        if(prediction_1 == 'Dislike'){
+            document.getElementById("result_emoji").innerHTML = "&#128078;";
+        }
+        else if(prediction_1 =='Clap'){
+            document.getElementById('result_emoji').innerHTML = "&#128079;";
+        }
+        else if(prediction_2 =='Wist'){
+            document.getElementById('result_emoji').innerHTML = "&#9994;";
+        }
+        else{
+            document.getElementById('result_emoji').innerHTML = "&#128076;";
+        }
+
+
+        if(prediction_2 == 'Dislike'){
+            document.getElementById("result_emoji2").innerHTML = "&#128522;";
+        }
+        else if(prediction_2 =='Clap'){
+            document.getElementById('result_emoji2').innerHTML = "&#128079;";
+        }
+        else if(prediction_2 =='Wist'){
+            document.getElementById('result_emoji2').innerHTML = "&#9994;";
+        }
+        else{
+            document.getElementById('result_emoji2').innerHTML = "&#128076;";
+        }
+    
+    }
+}
+
+
+ 
